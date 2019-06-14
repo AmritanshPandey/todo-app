6a2ac32bed65c2a297a0cf397b7d762a9df92be5
Amritansh Pandey <amritansh.pandey6@gmail.com>
2019-06-14 19:58:49 +0530

    Django-Initialization

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\amritansh\desktop\projects\learning-django-python\todo-app\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
