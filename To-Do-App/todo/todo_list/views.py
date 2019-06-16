f277a63272ee44295095b4463fefdd666adc9557
Amritansh Pandey <amritansh.pandey6@gmail.com>
2019-06-16 14:13:32 +0530

    added table styling

--- To-Do-App/todo/todo_list/views.py
+++ To-Do-App/todo/todo_list/views.py
@@ -9,10 +9,16 @@ def home(request):
 
          if form.is_valid():
              form.save()
-             all_items = List.objectives.all
-             messages.success(request, ('Item Has Been Added To List!'))
+             all_items = List.objects.all
+             messages.success(request, ('Item has been added to the list!'))
              return render(request, 'home.html', {'all_item': all_items})
 
      else:
          all_items = List.objects.all
          return render(request, 'home.html', {'all_items' : all_items})
+
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ('Item has been deleted!'))
